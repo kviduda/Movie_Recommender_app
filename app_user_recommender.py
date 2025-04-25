f2f75da866a61a296136337226490172dbfac10a
Kalyani Vidudala <kalyanividudala@gmail.com>
2025-04-24 20:45:54 -0400

    Update app_user_recommender.py

--- app_user_recommender.py
+++ app_user_recommender.py
@@ -40,8 +40,11 @@ def recommend_for_user(user_id, model, movies_df, ratings_df, n=5):
 # Load data and model
 movies = pd.read_csv("movies.csv")
 ratings = pd.read_csv("ratings.csv")
-with open("svd_model.pkl", "rb") as f:
+import os
+model_path = os.path.join(os.path.dirname(__file__), "svd_model.pkl")
+with open(model_path, "rb") as f:
     model = pickle.load(f)
+
 
 # Streamlit UI
 st.title("🎬 Personalized Movie Recommendations")
